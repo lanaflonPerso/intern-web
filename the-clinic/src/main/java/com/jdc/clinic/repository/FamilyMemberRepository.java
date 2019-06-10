2c62ebef333118a510f20d13fb81ab5ef4d5d0d9
Soe <tonepineapple997@gmail.com>
2019-06-10 23:22:40 +0630

    Repo

---
+++ the-clinic/src/main/java/com/jdc/clinic/repository/FamilyMemberRepository.java
@@ -0,0 +1,9 @@
+package com.jdc.clinic.repository;
+
+import com.jdc.clinic.customrepo.BaseRepository;
+import com.jdc.clinic.entity.FamilyMember;
+
+
+public interface FamilyMemberRepository extends BaseRepository<FamilyMember,Long>{
+
+}

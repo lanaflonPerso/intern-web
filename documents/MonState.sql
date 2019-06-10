305d7344ab71fc0cf03e7fb88ca7ec1d4a08de6a
ep <eiphyuphyuthant97@gmail.com>
2019-06-10 16:29:40 +0630

    update division

---
+++ documents/MonState.sql
@@ -0,0 +1,8 @@
+insert into township (division_id,name) values (9,'Mawlamyine');
+insert into township (division_id,name) values (9,'Thaton');
+insert into township (division_id,name) values (9,'Thanbyuzayat');
+insert into township (division_id,name) values (9,'Ye');
+insert into township (division_id,name) values (9,'Kyaikmaraw');
+insert into township (division_id,name) values (9,'Kyaikhto');
+insert into township (division_id,name) values (9,'Bakwai');
+insert into township (division_id,name) values (9,'Chaungzon');

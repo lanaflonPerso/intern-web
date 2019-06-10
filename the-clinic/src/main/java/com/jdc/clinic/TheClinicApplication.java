d90ea00d2bff0617fe3b87640167660534900092
minlwin <lwin.zawmin@gmail.com>
2019-06-10 13:48:00 +0630

    Share Project

---
+++ the-clinic/src/main/java/com/jdc/clinic/TheClinicApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheClinicApplication.class, args);
+	}
+
+}
---
+++ the-clinic/src/main/java/com/jdc/clinic/config/SecurityConfiguration.java
@@ -0,0 +1,5 @@
+package com.jdc.clinic.config;
+
+public class SecurityConfiguration {
+
+}

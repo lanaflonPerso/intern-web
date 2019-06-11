025104d48c886159803eda1b96211fcdeae68d7c
MSI GAMING <MSI GAMING@MSI>
2019-06-11 17:00:21 +0630

    u

--- the-clinic/src/main/java/com/jdc/clinic/controller/partner/ClinicController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/partner/ClinicController.java
@@ -1,51 +1,41 @@
 package com.jdc.clinic.controller.partner;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.jdc.clinic.entity.Clinic;
 
 @Controller
 @RequestMapping("/partner/clinics")
 public class ClinicController {
-	
-	@GetMapping("/create")
-	public String createClinic(Clinic clinic) {
-		//TODO
-		return "/views/partner/clinic_create";
-		
-	} 
-	
-	@PostMapping("/retrieve")
-	public String retrieveClinicById(int id) {
-		//TODO
-		return "/views/partner/clinic_retrieve";
-	}
-	
-	
-	@PostMapping("/update")
-	public String updateClinicById(int id) {
-		//TODO
-		return "/views/partner/clinic_update";
-	}
-	
-	
-	@PostMapping("/delete")
-	public String deleteClinicById(int id) {
-		//TODO
-		return "/views/partner/clinic_delete";
-		
-	}
-	
-	
+
+
 	@GetMapping("{id}")
-	public String searchByName(@PathVariable int id,@RequestParam("name") String name) {
-		//TODO 
-		return "redirect:/partner/clinic_name";
+	public String findById(@PathVariable int id,ModelMap model) {
+		return "/views/partner/clinic";
 	}
-	
+
+	@GetMapping("create")
+	public String createClinic() {
+		return "/views/partner/edit-clinic";
+	}
+
+	@PostMapping
+	public String save(Clinic clinic) {
+		return String.format("redirect:/partner/clinics/%d", clinic.getId());
+	}
+
+	@GetMapping("{id}/edit")
+	public String edit(@PathVariable int id) {
+		return "/views/partner/edit-clinic";
+	}
+
+	@PostMapping("{id}/delete")
+	public String deleteClinicById(@PathVariable int id) {
+		return String.format("redirect:/partner/clinics/%d", id);
+	}
+
 }

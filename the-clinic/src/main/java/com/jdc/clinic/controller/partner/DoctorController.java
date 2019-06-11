72423eebfe9f3ce13e655c9782388996e0902eab
WinTheingi <WinTheingi@DESKTOP-LQ2E51C>
2019-06-11 14:15:57 +0630

    doctor controller

--- the-clinic/src/main/java/com/jdc/clinic/controller/partner/DoctorController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/partner/DoctorController.java
@@ -1,10 +1,31 @@
 package com.jdc.clinic.controller.partner;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/partner/doctors")
 public class DoctorController {
+	
+	@GetMapping
+	public String index() {
+		
+		return "/views/partner/doctors";
+	}
+	
+	@PostMapping
+	public String create() {
+	
+		return "/views/partner/doctors";
+	}
+
+	@GetMapping("/{id}")
+	public String selectDoctor(@PathVariable int id) {
+		return "view/partner/doctorDetail";
+	}
+	
 
 }

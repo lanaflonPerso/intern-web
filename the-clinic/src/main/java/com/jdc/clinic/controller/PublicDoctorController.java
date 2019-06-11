6f99292106bb076cccd0a4b71b424d4c9c1795af
ChitThae <chitthae3131@gmail.com>
2019-06-11 13:59:06 +0630

    update public doctor

--- the-clinic/src/main/java/com/jdc/clinic/controller/PublicDoctorController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/PublicDoctorController.java
@@ -1,10 +1,16 @@
 package com.jdc.clinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/doctors")
 public class PublicDoctorController {
+	@GetMapping
+	public String search(String keyword,ModelMap model) {
+		return "/view/speciality/doctorlist";
+	}
 
 }

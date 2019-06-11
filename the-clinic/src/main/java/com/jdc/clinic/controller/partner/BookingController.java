ba5138aa1ad96aed7bdae3f7911f8ca5b32bfe31
DELL <DELL@DESKTOP-2HHK3G0>
2019-06-11 13:44:58 +0630

    commit BookingController and bookings.html

--- the-clinic/src/main/java/com/jdc/clinic/controller/partner/BookingController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/partner/BookingController.java
@@ -1,10 +1,25 @@
 package com.jdc.clinic.controller.partner;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/partner/bookings")
 public class BookingController {
+	
+	@GetMapping("/")
+	public String showBookingList() {
+		
+		return "/views/partner/bookings";
+	}
+	
+	
+	@GetMapping("/{id}")
+	public String selectPatient(@PathVariable int id) {
+		return "/views/partner/patientDetail";
+	}
 
 }

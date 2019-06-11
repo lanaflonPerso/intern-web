7fb1601dfb84d2dc90270349ad7e63dacd9b2268
Soe <tonepineapple997@gmail.com>
2019-06-11 13:32:05 +0630

    member booking controller

--- the-clinic/src/main/java/com/jdc/clinic/controller/member/MemberBookingController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/member/MemberBookingController.java
@@ -1,10 +1,43 @@
 package com.jdc.clinic.controller.member;
 
 import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member/bookings")
-public class MemberBookingController {
-
-}
+public class MemberBookingController {  
+	
+	
+	@GetMapping("/schedules")
+	public String viewSchedu() {
+		
+		return "views/schedules";
+	}
+	
+	@PostMapping("/book")
+	public String book() {
+		return "view/bookingForm";
+	}
+	
+	
+	@GetMapping("/bookings")
+	public String chooseMember(){
+		return "view/bookingFrom";
+		
+	} 
+	@PostMapping("/confirm")
+		public String confirmBooking() {
+			return "view/successBooking";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	

66e3c734262589d7332402e973be009b4292e8e3
Htet Aung Lin <halucsmub@gmail.com>
2019-06-11 13:13:33 +0630

    Patient Controller

--- the-clinic/src/main/java/com/jdc/clinic/controller/partner/PatientController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/partner/PatientController.java
@@ -1,10 +1,45 @@
 package com.jdc.clinic.controller.partner;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jdc.clinic.entity.Patient;
 
 @Controller
 @RequestMapping("/partner/patients")
 public class PatientController {
 
+	@GetMapping
+	public String index() {
+		return "view/partner/patients";
+	}
+
+	@RequestMapping("/search")
+	public String searchPatientByName(@RequestParam("name") String keyword) {
+		return "view/partner/patients";
+	}
+
+	@GetMapping("/{id}")
+	public String detailByID(@PathVariable int id) {
+		return "view/partner/patientDetail";
+	}
+
+	@PostMapping("/new")
+	public String create(Patient patient) {
+		return "view/partner/create";
+	}
+
+	@GetMapping("/update")
+	public String update(int id, Patient patient) {
+		return "view/partner/create";
+	}
+
+	@RequestMapping("/delete")
+	public String deleteByID(@RequestParam("id") int id) {
+		return "view/partner/patients";
+	}
 }

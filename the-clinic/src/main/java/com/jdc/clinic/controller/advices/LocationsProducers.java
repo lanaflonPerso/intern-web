73a5a57bc985a9729873e6359a2b78dd5991fe1a
minlwin <lwin.zawmin@gmail.com>
2019-06-13 19:30:41 +0630

    Change Scope for Location Producer

--- the-clinic/src/main/java/com/jdc/clinic/controller/advices/LocationsProducers.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/advices/LocationsProducers.java
@@ -2,21 +2,32 @@ package com.jdc.clinic.controller.advices;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.jdc.clinic.entity.Division;
 import com.jdc.clinic.services.LocationService;
 
+@Scope("application")
 @ControllerAdvice
 public class LocationsProducers {
 
 	@Autowired
 	private LocationService service;
 	
+	private List<Division> divisions;
+	
+	@PostConstruct
+	private void init() {
+		divisions = service.findValid();
+	}
+	
 	@ModelAttribute("divisions")
 	public List<Division> divisions() {
-		return service.findValid();
+		return divisions;
 	}
 }

cfa734eea79526cfaf4697ca5d3752043036c6c2
ep <eiphyuphyuthant97@gmail.com>
2019-07-03 16:29:25 +0630

    updating partner home controller

--- the-clinic/src/main/java/com/jdc/clinic/controller/partner/PartnerHomeController.java
+++ the-clinic/src/main/java/com/jdc/clinic/controller/partner/PartnerHomeController.java
@@ -1,5 +1,9 @@
 package com.jdc.clinic.controller.partner;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -9,8 +13,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jdc.clinic.entity.Booking;
 import com.jdc.clinic.entity.Clinic;
 import com.jdc.clinic.entity.Partner;
+import com.jdc.clinic.services.BookingService;
 import com.jdc.clinic.services.PartnerService;
 
 @Controller
@@ -20,10 +26,13 @@ public class PartnerHomeController {
 	@Autowired
 	PartnerService pService;
 
+	@Autowired
+	BookingService bookingService;
+
 	@GetMapping
 	public String index(ModelMap model, HttpServletRequest request) {
 		HttpSession session = request.getSession(true);
-
+		List<Booking> todayBookingList = new ArrayList<Booking>();
 		Partner partner = (Partner) session.getAttribute("partnerUser");
 
 		long totalBookingCount = 0;
@@ -35,11 +44,20 @@ public class PartnerHomeController {
 			totalDoctorCount += pService.getDoctorCountByClinicID(c.getId());
 		}
 
+		for (Booking b : bookingService.listAllBookings()) {
+
+			if (b.getBookingDate().compareTo(LocalDate.now()) == 0) {
+				todayBookingList.add(b);
+			}
+		}
+
 		model.addAttribute("clinicCount", pService.countClinicByPartner(partner.getPhone()));
 		model.addAttribute("bookingCount", totalBookingCount);
 		model.addAttribute("patientCount", totalPatientCount);
 		model.addAttribute("doctorCount", totalDoctorCount);
+		model.addAttribute("todayBookingList", todayBookingList);
 
 		return "/views/partner/home";
 	}
+
 }

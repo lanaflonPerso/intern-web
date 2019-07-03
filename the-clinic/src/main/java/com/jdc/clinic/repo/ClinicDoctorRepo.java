553af43277aa9685141ece92be0680f01ac171d2
ChitThae97 <chitthae3131@gamil.com>
2019-07-03 10:35:31 +0630

    update doctors controller

--- the-clinic/src/main/java/com/jdc/clinic/repo/ClinicDoctorRepo.java
+++ the-clinic/src/main/java/com/jdc/clinic/repo/ClinicDoctorRepo.java
@@ -2,14 +2,16 @@ package com.jdc.clinic.repo;
 
 import java.util.List;
 
+
+
 import com.jdc.clinic.entity.ClinicDoctor;
 import com.jdc.clinic.entity.ClinicDoctorPK;
-import com.jdc.clinic.entity.Doctor;
+
 import com.jdc.clinic.repo.custom.BaseRepository;
 
 public interface ClinicDoctorRepo extends BaseRepository<ClinicDoctor, ClinicDoctorPK> {
 
-	List<Doctor> findDoctorByClinicId(int clinicID);
+	List<ClinicDoctor> findByClinicId(int clinicID);
 
 	Long countDoctorByClinicId(int clinicID);
 }
--- the-clinic/src/main/java/com/jdc/clinic/services/DoctorService.java
+++ the-clinic/src/main/java/com/jdc/clinic/services/DoctorService.java
@@ -1,9 +1,11 @@
 package com.jdc.clinic.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jdc.clinic.entity.Doctor;
 import com.jdc.clinic.repo.ClinicDoctorRepo;
@@ -12,9 +14,15 @@ import com.jdc.clinic.repo.ClinicDoctorRepo;
 public class DoctorService {
 
 	@Autowired
-	ClinicDoctorRepo cdRepo;
-
-	public List<Doctor> getDoctorsByClinicId(int clinicID) {
-		return cdRepo.findDoctorByClinicId(clinicID);
+	private ClinicDoctorRepo cdRepo;
+	
+	@Transactional
+	public List<Doctor> getDoctorsByClinicId(int id){
+		return cdRepo.findByClinicId(id).stream()
+				.filter(cd -> cd.getClinic().getId()==id)
+				.map(cd->cd.getDoctor())
+				.collect(Collectors.toList());
 	}
+
+
 }

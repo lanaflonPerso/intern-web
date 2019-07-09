d1f7b304eb8cb4f95985fc4b93852b938c31aefc
DELL <DELL@DESKTOP-2HHK3G0>
2019-07-09 13:43:45 +0630

    commit booking repo

--- the-clinic/src/main/java/com/jdc/clinic/repo/BookingRepo.java
+++ the-clinic/src/main/java/com/jdc/clinic/repo/BookingRepo.java
@@ -1,5 +1,6 @@
 package com.jdc.clinic.repo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,12 @@ public interface BookingRepo extends BaseRepository<Booking, Long> {
 	@Query(value = "select count(*) from booking join clinic on booking.clinic_id = clinic.id where clinic.owner_phone = :phone", nativeQuery = true)
 	Long countBookingByPartnerPhone(String phone);
 
+	@Query(value = "select booking.* from booking join patient on booking.patient_id = patient.id join family_member on family_member.id = patient.family_member_id where member_phone= :phone and date(booking.booking_date)>=date(now())", nativeQuery = true)
+	List<Booking> findBookingByMemberPhoneAndDate(String phone);
+
+	@Query(value = "select booking.* from booking join patient on booking.patient_id = patient.id join family_member on family_member.id = patient.family_member_id where member_phone= :phone and date(booking.booking_date)=:date", nativeQuery = true)
+	List<Booking> findbookingByDate(String phone, LocalDate date);
+
+	@Query(value = "select count(*) from booking join patient on booking.patient_id = patient.id join family_member on family_member.id = patient.family_member_id where member_phone=:phone", nativeQuery = true)
+	Long countBookingByMemberPhone(String phone);
 }

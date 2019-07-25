4186e0a147ae0395366586e2eb97da57ba4e5575
Soe <tonepineapple997@gmail.com>
2019-07-25 17:19:55 +0630

    query

--- the-clinic/src/main/java/com/jdc/clinic/repo/TimeTableRepo.java
+++ the-clinic/src/main/java/com/jdc/clinic/repo/TimeTableRepo.java
@@ -2,11 +2,16 @@ package com.jdc.clinic.repo;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 import com.jdc.clinic.entity.Timetable;
 import com.jdc.clinic.repo.custom.BaseRepository;
 
-public interface TimeTableRepo extends BaseRepository<Timetable, Long>{
+public interface TimeTableRepo extends BaseRepository<Timetable, Long> {
 
 	List<Timetable> findByClinicDoctorClinicId(int id);
+
+	@Query(value = "update Timetable t set t.security.delete = true")
+	void deleteTimeTable(long id);
 
 }
